edbc25de78ddbd3b4293fef5f6098e2bc7fe3862
iuri sajaia <sajaiaiuri@gmail.com>
2023-05-31 15:55:12 +0400

    fix(TRANSLATION): add translation to user roles list

--- app/Filament/Resources/UserRoleResource/Pages/ListUserRoles.php
+++ app/Filament/Resources/UserRoleResource/Pages/ListUserRoles.php
@@ -9,10 +9,12 @@ use Filament\Resources\Pages\ListRecords;
 class ListUserRoles extends ListRecords
 {
     protected static string $resource = UserRoleResource::class;
+    use ListRecords\Concerns\Translatable;
 
     protected function getActions(): array
     {
         return [
+            Actions\LocaleSwitcher::make(),
             Actions\CreateAction::make(),
         ];
     }
