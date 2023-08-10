d3348f3846e8a55bf1eaa0fbacc6ede514863a81
iuri sajaia <sajaiaiuri@gmail.com>
2023-08-10 14:43:10 +0400

    feat(CSV): import drivers from csv file

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +16,19 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->fields([
+                    ImportField::make('name')
+                        ->label('Name')
+                        ->helperText('Define as user helper'),
+                    ImportField::make('email')
+                        ->label('Email'),
+                    ImportField::make('phone')
+                        ->label('Phone'),
+                    ImportField::make('user_role_id')
+                        ->label('User Role'),
+
+                ])
         ];
     }
 }
