689edc673155cc3f154c259f10e91d1776d82688
iuri sajaia <sajaiaiuri@gmail.com>
2023-10-05 15:43:28 +0400

    fix(AUTH): remove auth for types

--- routes/car.php
+++ routes/car.php
@@ -6,10 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::group(['middleware' => 'auth:sanctum','prefix' => 'car'], function () {
-    Route::post('/create' , [CarController::class , 'create']);
+Route::group(['prefix' => 'car'], function () {
     Route::get('/types', [CarTypeController::class , 'index']);
-    Route::post('/make-it-default/{id}', [CarController::class , 'makeItDefault']);
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::post('/create', [CarController::class , 'create']);
+        Route::post('/make-it-default/{id}', [CarController::class , 'makeItDefault']);
+    });
 });
 
 
--- routes/trailer.php
+++ routes/trailer.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\Api\TrailerTypeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware' => 'auth:sanctum','prefix' => 'trailer'], function () {
-    Route::post('/create' , [TrailerController::class , 'create']);
+Route::group(['prefix' => 'trailer'], function () {
     Route::get('/types', [TrailerTypeController::class , 'index']);
-    Route::post('/make-it-default/{id}', [TrailerController::class , 'makeItDefault']);
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::post('/create', [TrailerController::class , 'create']);
+        Route::post('/make-it-default/{id}', [TrailerController::class , 'makeItDefault']);
+    });
 });
 
 
